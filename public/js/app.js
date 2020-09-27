1d1b415ddcebf94a1d1ae289b47ef89b011d7e32
Biju N Bhaskaran <bijunb3705@gmail.com>
2020-09-27 01:26:24 -0400

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault() // this will prevent the browser to refresh the page
 
     const location = input.value
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
     fetch(url).then((response) =>{
         response.json().then((data) => {
             if(data.error){
